f8a3be8f920c13c52839e7c7566eea01fbd1837a
zozo-url <zozo-url@protonmail.ch>
2019-09-26 15:54:43 +1200

    make nav

---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Nav =  () =>{
+  return (
+    <div className="Nav">
+        <h1>SINEAD OVERBYE</h1>
+        <h1>WORKS</h1>
+    </div>
+  )
+}
+
+export default Nav;
