39c06b6708221b6d9af8e9a74508f5d0f76a0ca9
zozo-url <zozo-url@protonmail.ch>
2019-09-26 21:40:00 +1200

    add css to Works component

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -4,19 +4,24 @@ import ReactHtmlParser from 'react-html-parser'
 const Works =  (props) =>{
   return (
     <div className="Works">
-        <h1>Works</h1>
+      <div className="work-list">
+        <h1>works</h1>
         {data.poems.map((poem, index) => {
           return <div>
             <p>{poem.title}</p>
           </div>
         })}
+      </div>
+        <div className="poems">
         {data.poems.map((poem, index) => {
-          return <div>
-            <h1>{poem.title}</h1>
-            <p>{poem.publisher}, {poem.year}</p>
-            <p>{ReactHtmlParser(poem.poem)}</p>
-          </div>
-        })}
+            return <div>
+              <h1>{poem.title}</h1>
+              <p>{poem.publisher}, {poem.year}</p>
+              <p className="poem">{ReactHtmlParser(poem.poem)}</p>
+            </div>
+         })}
+        </div>
+
     </div>
   )
 }
