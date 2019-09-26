eec806afbdeadaac523fd19d646b83e7b870435c
zozo-url <zozo-url@protonmail.ch>
2019-09-26 17:44:07 +1200

    style home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,9 +4,9 @@ import { HashLink as Link } from 'react-router-hash-link';
 const Home =  () =>{
   return (
     <div className="Home">
-        <Link to="/about"><h2>about</h2></Link>
-        <h1>SINEAD OVERBYE</h1>
-        <Link to="/works"><h2>works</h2></Link>
+        <Link to="/about" className="link"><h2 Style="margin-bottom: 40vh">about</h2></Link>
+        <h1 Style="margin-bottom: 40vh">sinead overbye</h1>
+        <Link to="/works" className="link"><h2 Style="margin-top: 40vh">works</h2></Link>
     </div>
   )
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,10 +4,10 @@ import { HashLink as Link } from 'react-router-hash-link';
 const Nav =  (props) =>{
   return (
     <div className="Nav">
-        <Link to="/"><h3>SINEAD OVERBYE</h3></Link>
+        <Link to="/"  className="link"><h3>SINEAD OVERBYE</h3></Link>
         {props.match.url === "/works" ? 
-        <Link to="/about"><h3>ABOUT</h3></Link> : 
-        <Link to="/works"><h3>WORKS</h3></Link>}
+        <Link to="/about" className="link"><h3>ABOUT</h3></Link> : 
+        <Link to="/works" className="link"><h3>WORKS</h3></Link>}
     </div>
   )
 }
