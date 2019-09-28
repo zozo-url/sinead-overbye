7d19a3a8060deec4669b01cf8f806a9babde193d
zozo-url <zozo-url@protonmail.ch>
2019-09-28 16:33:00 +1200

    finish about page, make responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import sineadImg from '../sss.jpg'
+import 'font-awesome/css/font-awesome.min.css';
 
 const About =  (props) =>{
   return (
     <div className="About">
-        <div>
-          <img className="sinead-img" src={sineadImg} alt="Sinead Overbye" />
-          </div>
-        <div className= "about-description">
-          <p>Sinead Overbye (Te Aitanga-a-Māhaki, Rongowhakaata, Ngāti Porou, Te Ātiawa) is a writer and research assistant living in Wellington. She holds a BA in Art History and Spanish, and completed an MA at the International Institute of Modern Letters in 2018. Her poetry and fiction has been widely published. In 2019 she received a mentorship with the NZ Society of Authors, and is currently working on a novel. She will always be writing.</p>
-        {/* <p>social media links</p> */}
-        </div>
+        <img className="sinead-img" src={sineadImg} alt="Sinead Overbye"/>
+        <p>Sinead Overbye (Te Aitanga-a-Māhaki, Rongowhakaata, Ngāti Porou, Te Ātiawa) is a writer and research assistant living in Wellington. </p>
+          <p>She holds a BA in Art History and Spanish, and completed an MA at the International Institute of Modern Letters in 2018. Her poetry and fiction has been widely published. In 2019 she received a mentorship with the NZ Society of Authors, and is currently working on a novel. She will always be writing.</p>
+          <a href='https://twitter.com/applecarcass'><i className='fa fa-twitter'></i></a>
     </div>
   )
 }
