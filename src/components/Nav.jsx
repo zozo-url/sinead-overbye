43ca4f38a8df01aa8b56a9d37dd525f41a92b4a4
zozo-url <zozo-url@protonmail.ch>
2019-09-28 18:24:41 +1200

    make nav title a link

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,12 +7,12 @@ const Nav =  (props) =>{
        {props.match.url === "/about" ? 
        <div>
         <div className='link-space'><Link to="/about" className="link" Style="text-decoration: underline"><h2>about</h2></Link></div>
-        <div className='link-space'><h2 className= "nav-title">sinead overbye</h2></div>
+        <div className='link-space'><Link to="/" className="nav-link"><h2 className= "nav-title">sinead overbye</h2></Link></div>
         <div className='link-space'><Link to="/works" className="link"><h2>works</h2></Link></div>
         </div> :
         <div>
         <div className='link-space'><Link to="/about" className="link"><h2>about</h2></Link></div>
-        <div className='link-space'><h2 className= "nav-title">sinead overbye</h2></div>
+        <div className='link-space'><Link to="/" className="nav-link"><h2 className= "nav-title">sinead overbye</h2></Link></div>
         <div className='link-space'><Link to="/works" className="link" Style="text-decoration: underline"><h2>works</h2></Link></div>
         </div>}
     </div>
