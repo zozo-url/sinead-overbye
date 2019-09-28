35e947503ab2c67ac9dd1f28900e062fa96a71b1
zozo-url <zozo-url@protonmail.ch>
2019-09-28 12:02:47 +1200

    change about component css

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import sinead from '../ss.jpg'
+import sineadImg from '../sss.jpg'
 
 const About =  (props) =>{
   return (
     <div className="About">
         <div>
-          <img src={sinead} alt="Sinead Overbye" />
+          <img className="sinead-img" src={sineadImg} alt="Sinead Overbye" />
           </div>
-        <div>
-          <p>description of work</p>
-        <p>social media links</p></div>
+        <div className= "about-description">
+          <p>Sinead Overbye (Te Aitanga-a-Māhaki, Rongowhakaata, Ngāti Porou, Te Ātiawa) is a writer and research assistant living in Wellington. She holds a BA in Art History and Spanish, and completed an MA at the International Institute of Modern Letters in 2018. Her poetry and fiction has been widely published. In 2019 she received a mentorship with the NZ Society of Authors, and is currently working on a novel. She will always be writing.</p>
+        {/* <p>social media links</p> */}
+        </div>
     </div>
   )
 }
